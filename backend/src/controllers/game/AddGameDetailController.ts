48e0c2b4383d3563cf670f251c262a8ea4cb95f0
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-08-10 16:57:46 -0300

    Andamento

--- backend/src/controllers/game/AddGameDetailController.ts
+++ backend/src/controllers/game/AddGameDetailController.ts
@@ -1,22 +1,32 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express';
 import { AddGameDetailService } from '../../services/game/AddGameDetailService';
 
+class AddGameDetailController {
+  async handle(req: Request, res: Response) {
+    const { game_id, players } = req.body;
 
-class AddGameDetailController{
-  async handle(req: Request, res: Response){
-    const { game_id, player_id, score } = req.body;
+    const addGameDetail = new AddGameDetailService();
 
-    const addGameDetail= new AddGameDetailService();
+    const gameDetails = [];
 
-    const game = await addGameDetail.execute({
-      game_id,
-      player_id,
-      score
-    })
+    for (const player of players) {
+      const { player_id, score } = player;
 
-    return res.json(game);
+      try {
+        const gameDetail = await addGameDetail.execute({
+          game_id,
+          player_id,
+          score,
+        });
 
+        gameDetails.push(gameDetail);
+      } catch (error) {
+        return res.status(500).json({ error: 'Erro ao salvar Jogo!!' });
+      }
+    }
+
+    return res.json(gameDetails);
   }
 }
 
-export { AddGameDetailController }
+export { AddGameDetailController };
